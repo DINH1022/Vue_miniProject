28f385d8ac651d104a44a4c56995119cac5dfc52
22120433 <lequangvinh.127.2022@gmail.com>
2024-10-30 22:05:39 +0700

    first commit

---
+++ source/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h3>Home Page</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+}
+</script>
