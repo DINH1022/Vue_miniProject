17435a4dc1f02b85f53c6b3c34f6966523cc2ed8
22120433 <lequangvinh.127.2022@gmail.com>
2024-10-31 14:24:18 +0700

    get and databinding

--- source/src/views/products/List.vue
+++ source/src/views/products/List.vue
@@ -3,41 +3,53 @@
         <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
             <h2 class="display-4">Product Management</h2>
             <p><router-link to="/product/create">Add new</router-link></p>
-            </div>
+        </div>
 
-            <div class="container">
+        <div class="container">
             <div class="card-deck mb-3 text-center">
                 <table class="table table-bordered">
-                <thead>
-                    <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Product name</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                    </tr>
-                </tbody>
+                    <thead>
+                        <tr>
+                            <th scope="col" width="120">#</th>
+                            <th scope="col">Product name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr :key="index" v-for="(product, index) in products ">
+                            <th scope="row">{{ product.id }}</th>
+                            <td>{{ product.name }}</td>
+                            <td>{{ product.price }}</td>
+                            <td>
+                                <button class="btn btn-primary btn-small">Edit</button> &nbsp
+                                <button class="btn btn-danger btn-small">Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
-            </div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'ProductList',
+    data() {
+        return {
+            products: []
+        }
+    },
+    created() {
+        this.getAll();
+    },
+    methods: {
+        getAll() {
+            this.$request.get('http://localhost:8000/api/products').then(res => {
+                this.products = res.data
+            })
+        }
+    }
+}
+</script>
